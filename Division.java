55d162eb1e485eae7f4f2a7f148616e2df4805e1
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2023-09-19 16:24:40 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,15 @@
+/*
+File        :   Hellojava.java
+Description :   My First Java Program the "Hello Java"
+Author      :   Diya Benny
+Version     :   1.0
+Date        :   19/09/2023
+*/
+public class Division {
+	public static void main(String [] args) {
+		int number1=10,number2=5;
+		int Quo=number1/number2;
+		System.out.print("Quotient="+Quo);
+
+}
+}
---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+/*
+File        :   Hellojava.java
+Description :   My First Java Program the "Hello Java"
+Author      :   Diya Benny
+Version     :   1.0
+Date        :   19/09/2023
+*/
+public class Multiplication {
+	public static void main(String [] args) {
+		int number1=10,number2=4;
+		int prod=number1*number2;
+		System.out.print("Product="+prod);
+
+}
+
+}
