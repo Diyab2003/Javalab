5643badda4ddbbfb450d7b2581b26fd945110940
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2023-11-21 16:24:28 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class ExceptionDemo {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		char choice ='y';
+		while(choice=='y') {
+			System.out.println("Enter the first number");
+			int number1=sc.nextInt();
+			System.out.println("Enter the second number");
+			int number2=sc.nextInt();
+			try {
+				int result=number1/number2;
+				System.out.println("Result="+result);
+			}catch(Exception e) {
+				System.out.println("Division by zero is not possible!");
+			}
+			finally {
+				System.out.println("End of Operation");
+				System.out.println("Do you want to continue?");
+				choice=sc.next().charAt(0);
+			}
+			
+		}
+	}
+
+}
