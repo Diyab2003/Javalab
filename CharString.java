23a7d4b2e226241d7c5ef0313e66000b6bcaf796
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2023-11-29 11:27:56 +0530

    Add files via upload

---
+++ CharString.java
@@ -0,0 +1,18 @@
+package default_package;
+import java.util.Scanner;
+public class CharString {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the index");
+		int pos = sc.nextInt();
+		String input = "Java exercises!";
+		System.out.println("Original String = " +input);
+	
+			System.out.println("The character at position "+ pos + " is "+ input.charAt(pos));
+
+			
+			}
+		
+		
+	}
+	
