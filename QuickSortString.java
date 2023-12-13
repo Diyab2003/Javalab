217245b3c9807d43733191b59fc1a0f912f9aac4
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2023-12-13 14:46:54 +0530

    Update QuickSortString.java

--- QuickSortString.java
+++ QuickSortString.java
@@ -1,6 +1,12 @@
-import java.util.Scanner;
+/* File 	  : QuickSortDemo.java
+ * Description: QuickSort
+ * Author	  : Diya Benny
+ * Version	  : 1.0
+ * Date		  : 05/12/2023
+ */
 
-public class QuickSortString {
+import java.util.Scanner;
+public class QuickSortDemo {
     public static void swap(String[] arr, int i, int j) {
         String temp = arr[i];
         arr[i] = arr[j];
@@ -8,7 +14,7 @@ public class QuickSortString {
     }
 
     public static int partition(String[] arr, int low, int high) {
-        String pivot = arr[low]; // Select the first element as the pivot
+        String pivot = arr[low]; 
         int i = low;
         for (int j = low + 1; j <= high; j++) {
             if (arr[j].compareTo(pivot) < 0) {
@@ -16,15 +22,15 @@ public class QuickSortString {
                 swap(arr, i, j);
             }
         }
-        swap(arr, low, i); // Swap the pivot with the element at i
+        swap(arr, low, i); 
         return i;
     }
 
     public static void quickSort(String[] arr, int low, int high) {
         if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
         }
     }
 
@@ -32,19 +38,19 @@ public class QuickSortString {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the limit: ");
         int limit = sc.nextInt();
-        sc.nextLine(); // Consume the newline character
+        sc.nextLine(); 
 
         String[] strings = new String[limit];
-        System.out.println("Enter the strings to be sorted:");
+        System.out.println("Enter the strings:");
         for (int i = 0; i < limit; i++) {
             strings[i] = sc.nextLine();
         }
 
         quickSort(strings, 0, limit - 1);
 
-        System.out.println("Sorted strings:");
+        System.out.print("Sorted strings : ");
         for (int i = 0; i < limit; i++) {
-            System.out.println(strings[i]);
+            System.out.print(strings[i]+"  ");
         }
     }
 }
